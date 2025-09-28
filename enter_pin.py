ea4b73f1f477bd8cc344dddbb7960b7761b74fb5
ali2develop <muhammadaliamaan515@gmail.com>
2025-09-28 20:00:38 +0500

    Create enter_pin.py

---
+++ enter_pin.py
@@ -0,0 +1,21 @@
+# Write code below 💖
+
+print("MY BANK IN CODEDEX")
+
+pin = int(input("Enter your PIN: "))
+
+while pin != 1234:
+  pin = int(input("Incorrect PIN. Enter you Again: "))
+
+if pin == 1234:
+  print("PIN accepted !")
+
+
+
+
+
+
+
+
+
+
