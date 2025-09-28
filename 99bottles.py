ea6f31d7b3402e271fbf829b8a129c5d27319145
ali2develop <muhammadaliamaan515@gmail.com>
2025-09-28 20:50:54 +0500

    Create 99bottles.py

---
+++ 99bottles.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+
+for i in range(99,0,-1):
+  print(f"{i} bottles of beer on the wall ")
+  print(f"{i} bottles of beer")
+  print(f"Take one down, pass it around")
+  print(f"{i-1} bottles of beer on the wall")
